f44f4e3349ba7abab0ed2f0e0752d86c0b8a728d
Rechu <reszkettle@gmail.com>
2021-12-09 13:18:17 +0100

    Added method for creating vcards from entries

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 import requests
 from bs4 import BeautifulSoup
 import json
+import vobject
+
 app = FastAPI()
 
 
@@ -9,6 +11,22 @@ def search_results(query: str):
     response = requests.get(f"https://panoramafirm.pl/{query}")
     soup = BeautifulSoup(response.content, 'html.parser')
     found_results = soup.find_all('script', type='application/ld+json')[:-1]
-    entries = []
-    for result in found_results:
-        entries.append(json.loads(result.string))
+    return [json.loads(result.string) for result in found_results]
+
+
+def entries_to_vcards(entries: list[dict]):
+    for entry in entries:
+        card = vobject.vCard()
+        card.add('fn').value = entry['name']
+        card.add('email')
+        card.email.value = entry['email']
+        entry_telephone = entry.get('telephone', None)
+        if entry_telephone:
+            card.add('telephone').value = entry_telephone
+
+        entry_address = entry.get('address', None)
+        if entry_address:
+            address = card.add('adr')
+            address.value = vobject.vcard.Address(
+                street=entry_address.get('streetAddress', ''), city=entry_address.get('addressLocality', ''), code=entry_address.get('postalCode', ''), country=entry_address.get('addressCountry', ''))
+        print(card.serialize())
